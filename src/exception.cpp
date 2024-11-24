0be3e91f8d7430f0df8ff8a4ec5b148436bb4de2
louisb0 <louisbaragwanath@outlook.com>
2024-11-24 20:59:08 +1300

    Fix typo in access_error

--- src/exception.cpp
+++ src/exception.cpp
@@ -6,7 +6,7 @@ exception::exception(const std::string &message) : std::runtime_error(message) {
 exception::exception(const char *message) : std::runtime_error(message) {}
 
 type_error::type_error(const std::string &expected_type, const std::string &actual_type)
-    : exception("Attempted to read '" + actual_type + "' + as '" + expected_type + "'"),
+    : exception("Attempted to read '" + actual_type + "' as '" + expected_type + "'"),
       m_expected_type(expected_type), m_actual_type(actual_type) {}
 const std::string &type_error::expected_type() const noexcept { return m_expected_type; }
 const std::string &type_error::actual_type() const noexcept { return m_actual_type; }
